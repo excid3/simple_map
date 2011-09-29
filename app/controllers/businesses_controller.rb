3db30c488c7706651ab1d1b53d0a0cb7c9c7349e
Chris Oliver <excid3@gmail.com>
2011-09-28 23:04:46 -0500

    Loosen up authentication so people can see the index and businesses

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,5 +1,5 @@
 class BusinessesController < ApplicationController
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :except => [:index, :show]
 
   # GET /businesses
   def index
